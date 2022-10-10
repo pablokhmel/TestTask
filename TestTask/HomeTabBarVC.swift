9dc38fae480f0145ddea055637b3f3da1b701d60
pablokhmel <pablokhmelevskiy@gmail.com>
2022-10-10 22:13:37 +0200

    base commit

---
+++ TestTask/HomeTabBarVC.swift
@@ -0,0 +1,36 @@
+//
+//  HomeTabBarVC.swift
+//  TestTask
+//
+//  Created by MacBook on 10.10.2022.
+//
+
+import UIKit
+
+class HomeTabBarVC: UITabBarController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBackground
+        tabBar.tintColor = .label
+        setupVCs()
+    }
+
+    fileprivate func createNavController(for rootViewController: UIViewController,
+                                         title: String,
+                                         image: UIImage) -> UIViewController {
+        let navController = UINavigationController(rootViewController: rootViewController)
+        navController.tabBarItem.title = title
+        navController.tabBarItem.image = image
+        navController.navigationBar.prefersLargeTitles = true
+        rootViewController.navigationItem.title = title
+        return navController
+    }
+
+    func setupVCs() {
+        viewControllers = [
+            createNavController(for: FeedVC(), title: "Feed", image: UIImage(systemName: "photo")!),
+            createNavController(for: FavoriteVC(), title: "Favorite", image: UIImage(systemName: "star.fill")!)
+        ]
+    }
+}
